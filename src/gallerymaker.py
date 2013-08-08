bd3e2fb03760de8e4cd16ca63bb875fd6a4fec4b
Jim Richardson <weaselkeeper@gmail.com>
2013-08-07 22:25:39 -0700

    fleshing out the basics

--- src/gallerymaker.py
+++ src/gallerymaker.py
@@ -5,12 +5,21 @@ files in this directory """
 
 import os
 import sys
+import argparse
+
+def get_options():
+    """ command-line options """
+    parser = argparse.ArgumentParser(description='Pass cli options')
+    parser.add_argument('-d', '--dir', action = "store", dest = 'dir' )
+    args = parser.parse_args()
+    args.usage = 'gallerymaker [options]'
+    return args
 
 
-def get_videolist():
+def get_videolist(moviedir):
     """ Build a list of the video files """
     videolisting = []
-    vids = os.listdir('./')
+    vids = os.listdir(moviedir)
     for _file in vids:
         if _file.endswith('.m4v'):
             videolisting.append(_file)
@@ -25,6 +34,11 @@ def create_index(videolist):
     """ create the index.html, pointing at all the individual ones"""
 
 if "__main__" in __name__:
-    vids=get_videolist()
+    args = get_options()
+    if args.dir:
+        moviedir = args.dir
+    else:
+        moviedir = './'
+    vids = get_videolist(moviedir)
     print vids
 
