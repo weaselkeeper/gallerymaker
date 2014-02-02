deedfd51c0348e8ff7617afc0447fe3262fe2061
Jim Richardson <weaselkeeper@gmail.com>
2014-02-02 15:30:29 -0800

    minor delint

--- src/gallerymaker.py
+++ src/gallerymaker.py
@@ -31,16 +31,15 @@ def run():
     log.debug('In run function')
     vids = get_videolist(moviedir)
     # we assume moviedir is docroot.
-    log.debug('Creating top level index with %s, %s ' %
-            (vids, moviedir))
+    log.debug('Creating top level index with %s, %s ', vids, moviedir)
     create_index(vids, moviedir)
     if args.recurse:
         # find any subdirs that contain movie files
         for _dir in gallery_subdir(moviedir):
             filedir = os.path.join(moviedir, _dir)
             vids = get_videolist(filedir)
-            log.debug(' in run(), dealing with variables %s %s %s' %
-                    (filedir, vids, _dir))
+            log.debug(' in run(), dealing with variables %s %s %s',
+                    filedir, vids, _dir)
             create_index(vids, filedir, _dir)
     log.debug('leaving run()')
 
@@ -58,7 +57,7 @@ def gallery_subdir(_dir):
     if hasvids:
         if args.debug:
             for name in os.listdir(_dir):
-                log.debug('found %s in %s ' % (name, _dir))
+                log.debug('found %s in %s ', name, _dir)
         return [name for name in os.listdir(_dir)
             if os.path.isdir(os.path.join(_dir, name))]
     else:
@@ -102,30 +101,30 @@ def create_movie_html(videofile, _moviedir, subdir=''):
     # Write the index file for a movie to the topdir
 
     movie_index_page = moviedir + videofile.split('.')[0] + ".html"
-    log.debug('tracking some movies %s %s ' % (movie_index_page, videofile,))
+    log.debug('tracking some movies %s %s ', movie_index_page, videofile)
     movie_page = open(movie_index_page, 'w')
     if args.dryrun:
-        log.debug('writing header %s' % header)
+        log.debug('writing header %s', header)
     else:
         movie_page.write(header)
 
     moviefile_loc = os.path.join(subdir, videofile)
     if args.dryrun:
-        log.debug('writing moviepage %s' % moviefile_loc)
+        log.debug('writing moviepage %s', moviefile_loc)
     else:
         movie_page.write(moviefile_loc)
 
-    log.debug( 'moviefile location relative to docroot  %s' % moviefile_loc)
+    log.debug('moviefile location relative to docroot  %s', moviefile_loc)
 
     if args.dryrun:
-        log.debug('writing moviepage %s' % moviefile_loc)
+        log.debug('writing moviepage %s', moviefile_loc)
     else:
         movie_page.write(footer)
     movie_page.close()
     log.debug('leaving create_movie_html()')
 
 
-def create_index(videolist, _moviedir, subdir = '' ):
+def create_index(videolist, _moviedir, subdir=''):
     """ create the index.html, pointing at all the individual ones"""
     log.debug('entering create_index()')
     template_header = """
@@ -143,29 +142,29 @@ def create_index(videolist, _moviedir, subdir = '' ):
 """
     indexfile = os.path.join(_moviedir, 'index.html')
     if args.dryrun:
-        log.debug('writing header %s' % template_header)
+        log.debug('writing header %s', template_header)
     else:
         index = open(indexfile, 'w')
         index.write(template_header)
     if args.recurse:
         for _dir in gallery_subdir(_moviedir):
-            log.debug('found subdir %s containing 1+ movie files' % _dir)
+            log.debug('found subdir %s containing 1+ movie files', _dir)
             tag = '<a href=' + _dir + '/index.html>' + _dir + '</a></br>'
             if args.dryrun:
-                log.debug('writing tag %s' % tag)
+                log.debug('writing tag %s', tag)
             else:
                 index.write(tag)
     for video in videolist:
         videoname = video.split('.')[0]
         tag = '<a href="/' + videoname + '.html">' +  videoname + '</a></br>'
         if args.dryrun:
-            log.debug('writing tag %s' % tag)
+            log.debug('writing tag %s', tag)
         else:
             index.write(tag)
             index.write('\n')
         create_movie_html(video, _moviedir, subdir)
     if args.dryrun:
-        log.debug('writing footer %s' % template_footer)
+        log.debug('writing footer %s', template_footer)
     else:
         index.write(template_footer)
     index.close()
@@ -175,12 +174,12 @@ if "__main__" in __name__:
 
     # some command-line options
     parser = argparse.ArgumentParser(description='Pass cli options')
-    parser.add_argument('-c', '--config', action = "store",
+    parser.add_argument('-c', '--config', action="store",
         help='Specify a path to an alternate config file')
-    parser.add_argument('-d', '--dir', action = "store")
-    parser.add_argument('-r', '--recurse', action = "store_true")
-    parser.add_argument('-D', '--debug', action = 'store_true')
-    parser.add_argument('-n', '--dryrun', action = 'store_true')
+    parser.add_argument('-d', '--dir', action="store")
+    parser.add_argument('-r', '--recurse', action="store_true")
+    parser.add_argument('-D', '--debug', action='store_true')
+    parser.add_argument('-n', '--dryrun', action='store_true')
     args = parser.parse_args()
     args.usage = 'gallerymaker [options]'
 
